39640f6c14eac5aa68d5bc61bb7d7ea34fdc988e
tristandostaler <tristandostaler11@hotmail.com>
2023-04-05 02:14:41 -0400

    Fix

--- src/hooks/GoogleAPI/useUpdateLocalStateFromDrive.ts
+++ src/hooks/GoogleAPI/useUpdateLocalStateFromDrive.ts
@@ -4,7 +4,7 @@ import useStore from '@store/cloud-auth-store';
 import useLocalStore from '@store/store';
 import useGetFile from './useGetFile';
 
-const useUpdateLocalStateFromDrive = (isLoginProcess: boolean, setCurrentlySaving: Function) => {
+const useUpdateLocalStateFromDrive = (isLoginProcess: boolean, setCurrentlySaving: Function, isCurrentlySaving: Function) => {
     const getFile = useGetFile(isLoginProcess);
     const getCurrentChatIndex = () => { return useLocalStore.getState().currentChatIndex; }
     const setCurrentChatIndex = useLocalStore(state => state.setCurrentChatIndex);
@@ -12,11 +12,11 @@ const useUpdateLocalStateFromDrive = (isLoginProcess: boolean, setCurrentlySavin
     const getHideSideMenu = () => { return useLocalStore.getState().hideSideMenu; }
 
     const updateLocalStateFromDrive = () => {
-        setCurrentlySaving(true);
         getFile().then((fileContent) => {
-            if (!fileContent) {
+            if (!fileContent || isCurrentlySaving()) {
                 return;
             }
+            setCurrentlySaving(true);
             var state = JSON.parse(fileContent);
             var hsm = getHideSideMenu();
             var cci = getCurrentChatIndex();
