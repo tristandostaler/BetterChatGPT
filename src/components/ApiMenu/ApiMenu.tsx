bd84ee980f1ba92180c9ee5e27cf8913bf167dd5
Tristan Dostaler <>
2023-04-11 12:33:31 -0400

    Adding the possibility to specify the org id to use

--- src/components/ApiMenu/ApiMenu.tsx
+++ src/components/ApiMenu/ApiMenu.tsx
@@ -17,9 +17,12 @@ const ApiMenu = ({
   const setApiKey = useStore((state) => state.setApiKey);
   const apiEndpoint = useStore((state) => state.apiEndpoint);
   const setApiEndpoint = useStore((state) => state.setApiEndpoint);
+  const orgId = useStore((state) => state.orgId);
+  const setOrgId = useStore((state) => state.setOrgId);
 
   const [_apiKey, _setApiKey] = useState<string>(apiKey || '');
   const [_apiEndpoint, _setApiEndpoint] = useState<string>(apiEndpoint);
+  const [_orgId, _setOrgId] = useState<string>(orgId || '');
   const [_customEndpoint, _setCustomEndpoint] = useState<boolean>(
     !availableEndpoints.includes(apiEndpoint)
   );
@@ -28,6 +31,7 @@ const ApiMenu = ({
     setApiKey(_apiKey);
     setApiEndpoint(_apiEndpoint);
     setIsModalOpen(false);
+    setOrgId(_orgId);
   };
 
   const handleToggleCustomEndpoint = () => {
@@ -88,6 +92,20 @@ const ApiMenu = ({
           />
         </div>
 
+        <div className='flex gap-2 items-center justify-center mt-2'>
+          <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm'>
+            (Optional) Organisation ID:
+          </div>
+          <input
+            type='text'
+            className='text-gray-800 dark:text-white p-3 text-sm border-none bg-gray-200 dark:bg-gray-600 rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+            value={_orgId}
+            onChange={(e) => {
+              _setOrgId(e.target.value);
+            }}
+          />
+        </div>
+
         <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm flex flex-col gap-3 leading-relaxed'>
           <p className='mt-4'>
             <Trans
@@ -135,9 +153,8 @@ const ApiEndpointSelector = ({
       </button>
       <div
         id='dropdown'
-        className={`${
-          dropDown ? '' : 'hidden'
-        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90 w-32 w-full`}
+        className={`${dropDown ? '' : 'hidden'
+          } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90 w-32 w-full`}
       >
         <ul
           className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0'
--- src/store/auth-slice.ts
+++ src/store/auth-slice.ts
@@ -4,14 +4,17 @@ import { StoreSlice } from './store';
 export interface AuthSlice {
   apiKey?: string;
   apiEndpoint: string;
+  orgId: string;
   firstVisit: boolean;
   setApiKey: (apiKey: string) => void;
   setApiEndpoint: (apiEndpoint: string) => void;
+  setOrgId: (orgId: string) => void;
   setFirstVisit: (firstVisit: boolean) => void;
 }
 
 export const createAuthSlice: StoreSlice<AuthSlice> = (set, get) => ({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY || undefined,
+  orgId: '',
   apiEndpoint: defaultAPIEndpoint,
   firstVisit: true,
   setApiKey: (apiKey: string) => {
@@ -24,6 +27,12 @@ export const createAuthSlice: StoreSlice<AuthSlice> = (set, get) => ({
     set((prev: AuthSlice) => ({
       ...prev,
       apiEndpoint: apiEndpoint,
+    }));
+  },
+  setOrgId: (orgId: string) => {
+    set((prev: AuthSlice) => ({
+      ...prev,
+      orgId: orgId,
     }));
   },
   setFirstVisit: (firstVisit: boolean) => {
