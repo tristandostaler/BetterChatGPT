bf186ca5dcaa2fe72dd086b628f6207fd75b6fed
tristandostaler <tristandostaler11@hotmail.com>
2023-07-08 15:15:42 -0300

    Update prompt.ts

--- src/constants/prompt.ts
+++ src/constants/prompt.ts
@@ -27,6 +27,7 @@ https://www.google.com/search?q=query
 When using functions, make sure to respect the restrictions of each functions and the required parameters.
 If you receive an error, try something new.
 If it makes sense, display an image by using markdown image links.
+If needed, direct the user to type "/help" to get help on this app, list features, etc.
 `
 
 export const HelperPromptText: string = `
