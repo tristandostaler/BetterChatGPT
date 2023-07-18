ac8b9fd55b7e29b122f4ff4c55c0bb180f2716c8
tristandostaler <tristandostaler11@hotmail.com>
2023-07-18 00:22:25 -0400

    Update functions.ts

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -5,11 +5,13 @@ import { createClock } from "@openai_function_calling_tools/clock";
 import { createRequest } from "@openai_function_calling_tools/request";
 import { htmlToText } from 'html-to-text';
 import { createGoogleCustomSearch } from "@src/openai_function_calling_tools/googleCustomSearch";
+import { createSIEMUseCaseMetadata } from "@openai_function_calling_tools/siemUseCase";
 
 const [calculator, calculatorSchema] = createCalculator();
 const [clock, clockSchema] = createClock();
 const [request, requestSchema] = createRequest();
 const [googleCustomSearch, googleCustomSearchSchema] = createGoogleCustomSearch();
+const [siemUCMetadata, siemUCMetadataSchema] = createSIEMUseCaseMetadata();
 // const [webBrowser, webBrowserSchema] = createWebBrowser();
 
 const options = {
@@ -25,6 +27,7 @@ export const executeFunction = async (name: string, params?: any) => {
     if (name == "clock".toLowerCase()) return clock(params)
     if (name == "request".toLowerCase()) return htmlToText(await request(params), options)
     if (name == "googleCustomSearch".toLowerCase()) return await googleCustomSearch(params)
+    if (name == "SIEMUseCaseMetadata".toLowerCase()) return await siemUCMetadata(params)
     // if (name == "webbrowser") return await webBrowser(params)
     return null
 }
@@ -34,6 +37,7 @@ export const functionsSchemas = [
     clockSchema,
     requestSchema,
     googleCustomSearchSchema,
+    siemUCMetadataSchema,
     // webBrowserSchema,
 ]
 
